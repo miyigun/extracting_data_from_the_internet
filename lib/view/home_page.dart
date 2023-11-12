edaaeff1b49e274e72ed737bd0d4eb6307ad3319
Murat Iyigun <miyigun@hotmail.com>
2023-11-13 00:38:53 +0300

    Second commit

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -24,10 +24,57 @@ class HomePageState extends ConsumerState<HomePage>{
 
   @override
   Widget build(BuildContext context) {
+    var watch=ref.watch(controller);
+
     return Scaffold(
       appBar: AppBar(),
-      body: const Center(
-        child: Text(''),
+      body: Column(
+        children: [
+          const Text(
+              "Users",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          ListView.separated(
+            itemCount: watch.users.length,
+              itemBuilder: (BuildContext context, int index){
+              return Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      watch.users[index].avatar!
+                    ),
+                  ),
+                  title: Text(
+                    "${watch.users[index].firstName ?? ''} ${watch.users[index].lastName ?? ''}",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16,
+                    ),
+                  ),
+                  subtitle: Text(
+                    watch.users[index].email ?? '',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16,
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                ),
+              );
+              },
+              separatorBuilder: (BuildContext context, int index){
+                return const SizedBox(
+                  height: 15,
+                );
+              },
+
+          ),
+        ],
       ),
     );
   }
